ab683708dc6d61f3f60c44d771415ac4cee25f5f
GUSTAVO PAZ <gustavo.paz@portalsesisp.org.br>
2023-06-21 19:11:40 -0300

    hiding my ip

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', '192.168.15.12'],
+    domains: ['avatars.githubusercontent.com', 'meu ip'],
   },
 }
 
